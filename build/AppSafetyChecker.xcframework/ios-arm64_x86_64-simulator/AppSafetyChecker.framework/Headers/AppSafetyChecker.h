073f9d100c46667246baa6ece9797d9dc36501ac
ND <narendra.dhami@kuliza.com>
2021-06-03 08:16:58 +0530

    Initial commit

---
+++ build/AppSafetyChecker.xcframework/ios-arm64_x86_64-simulator/AppSafetyChecker.framework/Headers/AppSafetyChecker.h
@@ -0,0 +1,18 @@
+//
+//  AppSafetyChecker.h
+//  AppSafetyChecker
+//
+//  Created by Narendra Dhami on 25/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppSafetyChecker.
+FOUNDATION_EXPORT double AppSafetyCheckerVersionNumber;
+
+//! Project version string for AppSafetyChecker.
+FOUNDATION_EXPORT const unsigned char AppSafetyCheckerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppSafetyChecker/PublicHeader.h>
+
+
